3e5b03b258dd1649b9db9ebaee7039c3a609a2a6
MacFlaren <173524219+MacFlaren@users.noreply.github.com>
2025-01-18 19:30:31 +0200

    init

---
+++ 1. Programm start and console writing/Program.cs
@@ -0,0 +1,16 @@
+﻿// using це використовувати додаткову біблеотеку, system це біблеотека
+using System;
+
+// namespace це простір імен, project це назва простору імен
+namespace project { 
+    
+    class program
+    { // class це клас, program це назва класу
+        static void Main()
+        {
+            Console.WriteLine("Hello World!"); // Console.WriteLine це виведення тексту на екран    
+            Console.Read(); // Console.Read це зчитування тексту з клавіатури   
+        }
+    }
+
+}
